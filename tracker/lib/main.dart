62be4d9d01706be322c71035a191397342430470
bprabin811 <bprabin811@gmail.com>
2022-08-19 19:37:24 +0545

    second

--- tracker/lib/main.dart
+++ tracker/lib/main.dart
@@ -19,38 +19,39 @@ class Tracker extends StatefulWidget {
 class _TrackerState extends State<Tracker> {
 
   final DateTime date = DateTime.now();
-  ThemeData myMode=ThemeData.light();
+  //ThemeData myMode=ThemeData.light();
   String myName='There';
   bool isStart=false;
+  Color myColor=Colors.white;
   
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      darkTheme: myMode,
+     // darkTheme: myMode,
       title: 'Tracker',
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-      ),
+      
       home: Scaffold(   
-        backgroundColor: Colors.black,
+        backgroundColor: myColor,
         appBar: AppBar(
+          backgroundColor: myColor,
+          foregroundColor: Colors.grey[500],
           elevation: 5,
           title:Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Text(' Tracker',style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold
+               const Text(' Tracker',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
               ),),
-              myMode==ThemeData.light()?IconButton(onPressed: (){
+              myColor==Colors.white?IconButton(onPressed: (){
             setState(() {
-              myMode= ThemeData.dark();
+              myColor=Colors.black;
             });
           }, icon: const Icon( Icons.dark_mode)):
           IconButton(onPressed: (){
             setState(() {
-              myMode= ThemeData.light();
+              myColor=Colors.white;
             });
           }, icon: const Icon( Icons.light_mode)),
             ],
@@ -68,7 +69,7 @@ class _TrackerState extends State<Tracker> {
                   height: 120,
                   width: 400,
                   decoration: const BoxDecoration(
-                      color: Color.fromARGB(65, 233, 30, 98),
+                      color: Color.fromARGB(102, 220, 48, 105),
                       borderRadius: BorderRadius.all(Radius.circular(10)),
                     ),
                   
@@ -89,25 +90,39 @@ class _TrackerState extends State<Tracker> {
           ),
         ),
         drawer: Drawer(
+          backgroundColor: myColor,
           child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              children: const[
-                Padding(
-                  padding: EdgeInsets.only(bottom: 450),
-                  child: DrawerHeader(child: UserAccountsDrawerHeader(accountName: Text('DLTracker',style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),), accountEmail: Text('Count your steps.',style: TextStyle(fontSize: 15,)))),
+              children: [
+               Padding(
+                  padding: const EdgeInsets.only(bottom: 450),
+                  child: DrawerHeader(child:Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("DLTracker",style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[400]
+                      ),),
+                      Text("Count your steps.",style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey[400]
+                      ),),
+                    ],
+                  )),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(bottom: 10,right: 10),
+                  padding: const EdgeInsets.only(bottom: 10,right: 10),
                   child: ListTile(
                     subtitle: Text('It counts all step since phone is booted. There are no support of reset pedometer manually. To reset counter restart your device.'
-                    ,style: TextStyle(fontSize: 18),textAlign: TextAlign.justify,),
+                    ,style: TextStyle(fontSize: 18,color: Colors.grey[400]),textAlign: TextAlign.justify,),
                   ),
                 ),
                 ListTile(
-                  leading: Icon(Icons.copyright),
+                  leading: Icon(Icons.copyright,color: Colors.grey[400],),
                   title: Text('Pro Win'
-                  ,style: TextStyle(fontSize: 18),textAlign: TextAlign.justify,),
+                  ,style: TextStyle(fontSize: 18,color: Colors.grey[400]),textAlign: TextAlign.justify,),
                 ),
               ],
             ),
